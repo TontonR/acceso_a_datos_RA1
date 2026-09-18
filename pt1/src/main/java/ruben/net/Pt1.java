2faf0c29f2fb5db3ecfb995f91c6e0e416a40723
ruben <rumu439@vidalibarraquer.net>
2026-09-18 16:26:09 +0200

    first commit

---
+++ pt1/src/main/java/ruben/net/Pt1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ruben.net;
+
+import java.io.File;
+
+/**
+ *
+ * @author usuari-tarda
+ */
+public class Pt1 {
+    
+    public static void main(String[] args) {
+        File file = new File("data/ruben.txt");
+        //System.out.println(file.getAbsolutePath());
+        file
+    }
+}
