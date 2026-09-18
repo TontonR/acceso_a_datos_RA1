b0ca1f3f07b2a082dc17e04f55d117a19b1b1103
ruben <rumu439@vidalibarraquer.net>
2026-09-18 17:03:22 +0200

    1-2-3 acabats

--- pt1/src/main/java/ruben/net/Pt1.java
+++ pt1/src/main/java/ruben/net/Pt1.java
@@ -5,6 +5,8 @@
 package ruben.net;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -14,7 +16,58 @@ public class Pt1 {
     
     public static void main(String[] args) {
         File file = new File("data/ruben.txt");
-        //System.out.println(file.getAbsolutePath());
-        file
+        System.err.println("1. Total de caracters: " + count_characters(file)+1);
+        System.err.println("2. Total de linies: " + count_lines(file)+1);
+        System.err.println("3. Total de paraules: " + count_words(file)+1);    
+    }
+
+    public static int count_characters(File f){
+        try (FileReader fr = new FileReader(f)){
+            int c;
+            int total = 0;
+            while ((c = fr.read()) != -1){
+                if (c != '\n' && c != '\r'){
+                    total++;
+                    //char character = (char) c;
+                    // System.out.println(character);
+                }
+            }
+            return total;
+        } catch (IOException e) {
+            e.getStackTrace();
+            return -1;
+        }    
+    }
+
+    public static int count_lines(File f){
+        try (FileReader fr = new FileReader(f)){
+            int c;
+            int total = 0;
+            while ((c = fr.read()) != -1){
+                if (c == '\n' || c == '\r'){
+                    total++;
+                }
+            }
+            return total;
+        } catch (IOException e) {
+            e.getStackTrace();
+            return -1;
+        }    
+    }
+
+    public static int count_words(File f){
+        try (FileReader fr = new FileReader(f)){
+            int c;
+            int total = 0;
+            while ((c = fr.read()) != -1){
+                if (c == '\n' || c == '\r' || c == ' '){
+                    total++;
+                }
+            }
+            return total;
+        } catch (IOException e) {
+            e.getStackTrace();
+            return -1;
+        }    
     }
 }
