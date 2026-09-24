f5d5d1dbf5d8347c28faa8ebab0add4970c411ce
Ruben Muñoz Blanco <rumu439@vidalibarraquer.net>
2026-09-24 22:57:04 +0200

    pt1 acabada

--- pt1/src/main/java/ruben/net/Pt1.java
+++ pt1/src/main/java/ruben/net/Pt1.java
@@ -7,6 +7,8 @@ package ruben.net;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -19,7 +21,9 @@ public class Pt1 {
         System.err.println("1. Total de caracters: " + count_characters(file)+1);
         System.err.println("2. Total de linies: " + count_lines(file)+1);
         System.err.println("3. Total de paraules: " + count_words(file)+1);    
+        System.err.println("4. La lletra que més surt és: '" + most_frequent_letter(file).keySet().toArray()[0] + "' (" + most_frequent_letter(file).values().toArray()[0]+" vegades)");    
     }
+         
 
     public static int count_characters(File f){
         try (FileReader fr = new FileReader(f)){
@@ -70,4 +74,38 @@ public class Pt1 {
             return -1;
         }    
     }
+
+    public static Map<Character, Integer> most_frequent_letter(File f){
+    try (FileReader fr = new FileReader(f)){
+        Map<Character, Integer> letters = new HashMap<>();
+        int c;
+        while ((c = fr.read()) != -1){
+            char ch = (char) c;
+            if (ch != '\n' && ch != '\r' && ch != ' '){
+                ch = Character.toLowerCase(ch);
+                letters.put(ch, letters.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+        char max_char = ' ';
+        int max_freq = 0;
+        for (Map.Entry<Character, Integer> entry : letters.entrySet()) {
+            if (entry.getValue() > max_freq) {
+                max_freq = entry.getValue();
+                max_char = entry.getKey();
+            }
+        }
+
+        Map<Character, Integer> result = new HashMap<>();
+        if (max_freq > 0) { 
+            result.put(max_char, max_freq);
+        }
+
+        return result;
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+    }    
+}
+
 }
